626454645fef819ecb3a50d8208e4e89f9d06e6f
Chely Ho <adept_inept@yahoo.com>
2018-03-06 01:47:17 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,8 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-binding.pry
-    self.artists.names
-
+    self.artists.names.collect{|artist| artist.name}
   end
 end
